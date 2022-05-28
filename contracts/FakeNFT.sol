c3a57d554b5db170cdf8380111394396adf46f30
mr-abims <adebayoabimbola9@gmail.com>
2022-05-28 12:53:54 +0100

    transaction script done

--- contracts/FakeNFT.sol
+++ contracts/FakeNFT.sol
@@ -4,12 +4,13 @@ pragma solidity ^0.8.4;
 import "@openzeppelin/contracts/token/ERC721/ERC721.sol";
 contract FakeNFT is ERC721 {
     uint256 tokkenId = 1;
-    uint256 constant price = 0.001 ether;
+    uint256 constant price = 0.01 ether;
+    
     constructor() ERC721("FAKE", "FAKE") {
 
     }
 
-    function mint() public payablle {
+    function mint() public payable {
         require(msg.value == price, "Invalid ether amount");
         _mint(msg.sender, tokkenId);
         tokkenId +=1;
